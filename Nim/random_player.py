bea8baa2ada507021f03717dc206946d1c54a35f
KevinTD15 <talaveradiazkevin1999@gmail.com>
2023-06-11 23:30:18 -0500

    deleted

--- Nim/random_player.py
+++
@@ -1,9 +0,0 @@
-from Nim.player import player
-import random as rd
-
-class random_player(player):
-    def __init__(self, name) -> None:
-        super().__init__(name)
-        
-    def _select_move(self, sticks_list : list) -> int:
-        return rd.randint(1, 3)
